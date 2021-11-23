c3c8a798f89430113e17879f86ff262055434587
DileSoft <dilesoft@gmail.com>
2021-11-23 21:58:27 +0300

    moves history

--- index.js
+++ index.js
@@ -14,6 +14,53 @@ const
 	OG_REFRESH_ANSWER_PACKET = 7, // 7 - Ответ на 6й пакет
 	OG_ARCADE_GAME_PACKET = 8	  // 8 - Запуск тестовой версии с сервера
 
+const games = {
+  0: {
+    id: '0',
+    game_rnd: '0',
+    type: 'A',
+    laps: 5,
+    seeds: 200,
+    duration: 10,
+    move_cnt: 0,
+    steps_cnt: 0,
+    world_id: 0,
+    route_id: 0,
+    express: 'Y',
+    owner_id: '0',
+    players: {
+      0: {
+        id: '0',
+        name: 'player',
+        turns: [],
+        robot: 'N',
+      },
+      1: {
+        id: '1',
+        name: 'player2',
+        turns: [],
+        robot: 'N',
+      }
+    }
+  }
+};
+
+function getPlayersString(game) {
+  return Object.values(games[game].players).map(player => `${player.id};${player.name};1;1;1;1;${player.robot}`).join(';');
+}
+
+function getGameString(game) {
+  return `${games[game].id};0;0;0;password;`+
+  `${games[game].world_id};${games[game].route_id};${games[game].game_rnd};`+
+  `${games[game].type};${games[game].laps};${games[game].seeds};`+
+  `${games[game].duration};${games[game].move_cnt};${Object.values(games[game].players).length};`+
+  `${games[game].steps_cnt};${games[game].express}`;
+}
+
+function getSeedsString(game) {
+  return Object.values(games[game].players).map(player => player.turns[player.turns.length-1]).join(';');
+}
+
 function parseRequest(request) {
   let result = request.split(';');
   const lastIndex = result.findIndex(item => item.startsWith('BITRIX'));
@@ -27,7 +74,9 @@ app.get('*', (req, res) => {
   console.log('get');
   console.log(req.query);
   if (req.path === '/game-on-line/default.asp') {
-    res.send('KDLAB;104;0;0;0;0;0;password;0;0;0;A;10;10;200;10;2;1;Y;;;;;;;0;player;1;1;1;1;N;1;player2;1;1;1;1;N;BITRIX');
+    games[0].move_cnt=0;
+    games[0].steps_cnt=0;
+    res.send(`KDLAB;104;0;${getGameString(0)};;;;;;;${getPlayersString(0)};BITRIX`);
   } else {
     res.send('Hello World!')
   }
@@ -40,10 +89,17 @@ app.post('*', (req, res) => {
     //   res.send(req.body);
         const data = parseRequest(req.body);
         console.log(data);
-        if (data[2] === '6') {
+        if (parseInt(data[2]) === OG_REFRESH_PACKET) {
           console.log('go');
-            res.send('KDLAB;104;1;0;0;0;0;password;0;0;0;A;11;11;200;11;2;1;Y;;;;;;;0;player;1;1;1;1;N;1;player2;1;1;1;1;N;11;2;0;Y;0;0;0;0;0;0;0;2;283#402#51#-1#504#375#50#-1;1;Y;0;0;0;0;0;0;0;2;283#402#51#-1#504#375#50#-1;BITRIX');
-        } else {
+            res.send(`KDLAB;104;${OG_GAME_PACKET};${getGameString(0)};;;;;;;${getPlayersString(0)};${games[0].steps_cnt};2;${getSeedsString(0)};BITRIX`);
+        } else if (parseInt(data[2]) === OG_SEEDS_PACKET) {
+          games[0].move_cnt+=1;
+          games[0].steps_cnt+=1;
+          games[0].players[0].turns.push(data.slice(25).join(';'));
+          games[0].players[1].turns.push([1, ...data.slice(26)].join(';'));
+          console.log(getSeedsString(0));
+          res.send('OK:KDLAB');
+        } else if (parseInt(data[2]) === OG_CONTROL_PACKET) {
           res.send('OK:KDLAB');
         }
     // res.send('NEXT_MOVE');
